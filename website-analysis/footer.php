02245934315eb4a2901598a561a5d1399d58e104
TechWriterChris <157309187+TechWriterChris@users.noreply.github.com>
2024-01-22 13:30:34 -0700

    Add files via upload

---
+++ website-analysis/footer.php
@@ -0,0 +1,1 @@
+<p style="padding-top: 20px;">&copy; <?php echo date("Y"); ?> Christine Roeder</p>
---
+++ website-analysis/index.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Website Analysis</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta charset="utf-8">
+        <meta name="robots" content="noimageindex">
+        <meta name="description" content="Website Analysis of Tech News Today">
+        <meta name="author" content="Christine Roeder">
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Crimson+Pro">
+        <link rel="stylesheet" href="style.css">
+    </head>
+
+    <body>
+        <div id="main">
+            <h1>Analysis of <i>Tech News Today</i> Website</h1>
+            <div id="byline">by Christine Roeder, December 2022</div>
+
+            <h2>Summary</h2>
+            <p>Tech News Today publishes high-quality guides, tips and tricks, tutorials, and other content to help people learn about technology. Their articles cover hardware and software, all three of the major operating systems (Windows, Mac, and Linux), mobile devices, and gaming. There are additional sections related to IT (security and networking), as well as reviews and buyer's guides.</p>
+            <p>The website has a clean, simple design and its navigation is straightforward and easy to use. A plain color scheme of black and white with red accents is easy on the eyes and does not distract from the content. Each category page is arranged as a grid of articles, each containing an image, title, author, and date. A narrower content bar along the right edge displays three articles each for Latest Posts, You May Also Like, and Recommended.</p>
+            <p>All articles appear to use the same style guide that keeps layout and formatting consistent across the website. Because of this, I chose to base my analysis on a single page titled <a href="https://www.technewstoday.com/things-to-do-in-excel/" target="_blank">&ldquo;20 Things to Do in Excel That Will Make You an Expert&rdquo;</a>.</p>
+
+            <h2>Audience</h2>
+            <p>The audience is male and female working-age adults. Their ages range from 20 to 50 years, and they have full-time white-collar office jobs with annual salaries over $40,000. This audience wants to advance their skills with Excel spreadsheets to become more valuable to their employers.</p>
+
+            <h2>Accessibility</h2>
+            <p>After the introduction, the text includes a collapsible table of contents so the user can quickly see what the 20 topics are and click to move directly to the desired topic.</p>
+            <p>Each Excel function was explained succinctly and clearly in short paragraphs with bullet lists. I was able to locate each function as shown in the screenshots provided. Although this webpage is not a tutorial per se, a tutorial link was provided for some of the functions.</p>
+            <p><img src="images/screencap1.png" alt="Screenshot showing link, yellow highlighted box, and screen capture in Excel" class="responsive"></p>
+            <p>Clear headers are used to designate each section. The sans-serif font size, line height, and short sentences make the text easy to read. Headings and bold typeface make the document easy to scan (Loranger). Blocks with a yellow background lead the reader's eye to important information. Each image includes a descriptive alt tag to accommodate visually impaired readers.</p>
+            <p><img src="images/screencap2.png" alt="Screenshot showing heading and bold text" class="responsive"></p>
+            <p>The author provided written examples when a screenshot would not be very useful.</p>
+            <p><img src="images/screencap3.png" alt="Screenshot showing author's written example" class="responsive"></p>
+
+            <h2>Plain Language</h2>
+            <p>My search terms were "how to do advanced things in excel." Not counting the ads, this website ranked fifth in the search results. I viewed the page's source code and found no keywords specified in the header meta tags. The title and plain language in the text were sufficient to grant the page a high search engine ranking (Loranger).</p>
+            <p>Using the Document Stats function in Microsoft Word, the text has a 65.3 ranking for Flesch Reading Ease and 7.5 for the Flesch-Kinkaid Grade Level. These scores indicate that the webpage is appropriate for ages 13 to 15, which is considered suitable for the general public (Readable).</p>
+
+            <h2>Conclusion</h2>
+            <p>I was hard-pressed to identify opportunities for improvement on the Tech News Today website. The site has a consistent content theme and a clean layout. Its navigation is easy to use. The layout is functional and attractive. All pages use a single style guide, so the reader knows exactly what to expect from one article to the next. Surprisingly, I did not even find misspellings or grammatical errors. The authors, editors, and web developers appear to work collaboratively to produce a high-quality website.</p>
+            <p>The only recommendation I have is to review their website's compliance with national accessibility standards. I ran their top-level domain (technewstoday.com) through the <a href="https://www.accessibilitychecker.org/" target="_blank">online accessibility checker</a> on AccessibilityChecker.org. The results indicate that Tech News Today can improve its accessibility by improving the color contrast and font size for its breadcrumb element and article dates. It is also advisable for the organization to check images for color contrast and accessibility to those with color blindness.</p>
+
+            <h2>References</h2>
+            <p>Loranger, Hoa. <i>Plain Language Is for Everyone, Even Experts</i>. 8 October 2017. 4 December 2022. &lt;https://www.nngroup.com/articles/plain-language-experts/?lm=text-scanning-patterns-eyetracking&pt=article&gt;.</p>
+            <p>Readable. <i>Flesch Reading Ease and the Flesch Kincaid Grade Level</i>. n.d. 4 December 2022. &lt;https://readable.com/readability/flesch-reading-ease-flesch-kincaid-grade-level/&gt;.</p>
+            <p>Sibisi, Mduduzi. <i>20 Things to Do in Excel That Will Make You an Expert</i>. 24 September 2021. 4 December 2022. &lt;https://www.technewstoday.com/things-to-do-in-excel/&gt;.</p>
+
+            <div class="footer">
+                <?php include 'footer.php'; ?>
+            </div>
+        </div>
+    </body>
+</html>
